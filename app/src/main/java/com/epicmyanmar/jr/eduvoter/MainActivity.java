060c5ebdd26c888e169616621ea4aa0de074b1cf
winhtaikaung <winhtaikaung28@hotmail.com>
2015-05-29 03:25:57 +0630

    CardView Added

--- app/src/main/java/com/epicmyanmar/jr/eduvoter/MainActivity.java
+++ app/src/main/java/com/epicmyanmar/jr/eduvoter/MainActivity.java
@@ -3,16 +3,41 @@ package com.epicmyanmar.jr.eduvoter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import adapter.Cardview_Adapter;
 
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+
+        initialize();
+
+        listView = (ListView) findViewById(R.id.main_list);
+        List<String> datalist = Arrays.asList(new String[]{"item1","item2","item_3"});
+        Cardview_Adapter adp = new Cardview_Adapter(this, datalist);
+        listView.setAdapter(adp);
+
+    }
+    void initialize(){
+        setSupportActionBar(toolbar);
+
+    }
+    void BindData(){
+
     }
 
     @Override
