7ad5e0d290459181326921abe4bcdb39422bd81a
winhtaikaung <winhtaikaung28@hotmail.com>
2015-05-29 04:35:06 +0630

    Item 1 added

---
+++ app/src/main/java/com/epicmyanmar/jr/eduvoter/Item_1Activity.java
@@ -0,0 +1,100 @@
+package com.epicmyanmar.jr.eduvoter;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.Item1_Card_adapter;
+import model.VoteRules;
+
+
+public class Item_1Activity extends ActionBarActivity {
+    Toolbar toolbar;
+    ListView listView;
+
+    List<VoteRules> mVoteList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_1);
+        toolbar=(Toolbar)findViewById(R.id.item_1toolbar);
+
+        initialize();
+        demo_db();
+
+        listView=(ListView) findViewById(R.id.item1_list);
+        Item1_Card_adapter item1_card_adapter=new Item1_Card_adapter(this,mVoteList);
+        listView.setAdapter(item1_card_adapter);
+        listView.setOnItemClickListener(new item1_listCLickListener());
+    }
+    void initialize(){
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("How to Vote");
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        }
+
+
+    }
+
+
+    void demo_db(){
+        mVoteList=new ArrayList<VoteRules>();
+        VoteRules vr1=new VoteRules();
+        vr1.setHeader("Header 1");
+        vr1.setDescription("Lorem ispumLorem ispumLorem ispumLorem ispumLorem ispumLorem ispumLorem ispumLorem ispum");
+        VoteRules vr2=new VoteRules();
+        vr2.setHeader("Header 2");
+        vr2.setDescription("Lorem ispumLorem ispumLorem ispumLorem ispumLorem ispumLorem ispumLorem ispumLorem ispum");
+        VoteRules vr3=new VoteRules();
+        vr3.setHeader("Header 1");
+        vr3.setDescription("Lorem ispumLorem ispumLorem ispumLorem ispumLorem ispumLorem ispumLorem ispumLorem ispum");
+        mVoteList.add(vr1);
+        mVoteList.add(vr2);
+        mVoteList.add(vr3);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_item_1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected  class item1_listCLickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(Item_1Activity.this,""+position+"",Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/model/VoteRules.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ * Created by jr on 5/29/15.
+ */
+public class VoteRules  {
+    private String header;
+    private String description;
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
